952e8fbb8c25c78b1b2a817637a172edd4ea9b66
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2017-02-27 02:28:02 -0800

    Almost done, going to double check layout stuff on different sizes tomorrow

--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-class HangmanViewController: UIViewController {
+class HangmanViewController: UIViewController, UITextFieldDelegate {
     
     var hangman = HangmanModel();
-
+    let limitLength = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,12 +21,27 @@ class HangmanViewController: UIViewController {
         print(phrase)
         hangman.phrase = phrase
         
-        //initialize empty
+        //initialize empty HiddenWord
+        var hiddenText = ""
         for char in phrase.characters {
             if char != " " {
-                HiddenWord.text? += ("_  ")
+                hiddenText += "_  "
+            }
+            else {
+                hiddenText += "   "
             }
         }
+        HiddenWord.text = hiddenText
+        
+        //initialize original image
+        hangmanImage.image = UIImage(named: "hangman1")
+        
+        //initialize incorrect guesses
+        IncorrectGuesses.text = "Incorrect Guesses: "
+        
+        GuessEnter.delegate = self
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,26 +49,115 @@ class HangmanViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let text = textField.text else { return true }
+        let newLength = text.characters.count + string.characters.count - range.length
+        return newLength <= limitLength
+    }
+
     
    
+    
+    func updateHiddenWordText(guess: String, indices: [Int]) {
+        var newString = ""
+        var index = 0
+        for i in 0..<HiddenWord.text!.characters.count {
+            if (index < indices.count && i / 3 == indices[index]) {
+                newString.append(guess)
+                index += 1
+            }
+            else {
+                newString.append(HiddenWord.text!.substring(atIndex: i))
+            }
+        }
+        HiddenWord.text = newString
+        print(HiddenWord.text!.characters.count)
+    }
+    
+    func updateIncorrectGuessesText(guess: String) {
+        IncorrectGuesses.text!.append(" " + guess)
+    }
+    
+    func updateHangmanImage(lives: Int) {
+        let imageNum = 8 - lives
+        let name = "hangman" + String(imageNum)
+        hangmanImage.image = UIImage(named: name)
+    }
+    
+
     @IBAction func StartOverButton(_ sender: Any) {
+        hangmanImage.image = UIImage(named: "hangman1")
         hangman.reset()
+        viewDidLoad()
+    }
+
+    
+    @IBAction func GuessButtonEnter(_ sender: Any) {
+        //get input
+        if !hangman.finished {
+        if let input = GuessEnter.text {
+            GuessEnter.text = ""
+            let inputUpper = input.uppercased()
+            //change hidden word if needed
+            if hangman.containsGuess(guess: inputUpper) {
+                let indices = hangman.indicesOfLetter(letter: inputUpper)
+                print(indices)
+                updateHiddenWordText(guess: inputUpper, indices: indices)
+                hangman.incrCorrectLetters(indicesArr: indices)
+            }
+            else {
+                hangman.decreaseLives()
+                updateIncorrectGuessesText(guess: inputUpper)
+                updateHangmanImage(lives: hangman.lives);
+            }
+            if hangman.hasWon() {
+                hangman.finished = true;
+                let alert = UIAlertController(title: "Congrats!", message: "You've won!", preferredStyle: UIAlertControllerStyle.alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
+                    (result : UIAlertAction) -> Void in
+                    print("OK")
+                }
+                alert.addAction(okAction)
+                self.present(alert, animated: true)
+                print("has won")
+                //do game won stuff
+            }
+            if hangman.hasLost() {
+                hangman.finished = true;
+                let alert = UIAlertController(title: "Oops", message: "You've lost :(", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                print("has lost")
+                //do game lost stuff
+            }
+            
+        }
+        }
+        else {
+            
+        let alert = UIAlertController(title: "Oops", message: "Game over! Press Start Over to play again", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false;
     }
     
     
-    @IBOutlet weak var GuessEnter: UITextField!
-
-
-    @IBAction func GuessButton(_ sender: Any) {
-    }
     
-    @IBOutlet weak var IncorrectGuesses: UILabel!
+    @IBOutlet weak var hangmanImage: UIImageView!
     
     @IBOutlet weak var HiddenWord: UILabel!
     
+    @IBOutlet weak var IncorrectGuesses: UILabel!
+    
+    @IBOutlet weak var GuessEnter: UITextField!
+    
+        
     /*
      
     // MARK: - Navigation
--- Hangman/HangmanModel.swift
+++ Hangman/HangmanModel.swift
@@ -11,8 +11,9 @@ import UIKit
 class HangmanModel: UIViewController {
     
     var phrase: String = ""
-    var correctLetters = 0
-    
+    var correctLetters: Int = 0
+    var lives: Int = 7
+    var finished: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,13 +28,38 @@ class HangmanModel: UIViewController {
     //handle game over condition
     
     func reset() {
-        correctLetters = 0;
+        correctLetters = 0
+        lives = 7
+        finished = false;
     }
-    /* returns TRUE if the game is over (i.e, the number of correctly
-    guessed letters equals the number of letters in phrase*/
     
-    func gameOver() -> Bool {
-        return correctLetters == phrase.removingWhitespaces().characters.count
+    /* return true if the user has won, i.e the number of correct letters guessed
+    equals the number of characters in the Phrasee, minus whitespace. Note: correct
+    letters can be the same, i.e guessing "r" for Crossroads counts as 2 correct
+    letters*/
+    func hasWon() -> Bool {
+        return correctLetters == phrase.replacingOccurrences(of: " ", with: "").characters.count
+    }
+    
+    /* Return true if the user has lost, i.e the number of lives is now 0. Lives = 7 because there are 7 body parts (head, neck, body, left arm, right arm, left leg, right leg. */
+    func hasLost() -> Bool {
+        return lives == 1
+    }
+    
+    /*Returns TRUE if the Hidden Word need to be updated, else FALSE */
+    func changeHiddenWord(guess: String) -> Bool {
+        return containsGuess(guess: guess)
+    }
+    
+    
+    /*Returns TRUE if the Incorrect Guesses need to be updated, else FALSE */
+    func changeIncorrectGuesses(guess: String) -> Bool{
+        return !containsGuess(guess: guess)
+    }
+    
+    /*Returns TRUE if the image needs to be updated, else FALSE */
+    func changeImage(guess: String) -> Bool {
+        return !containsGuess(guess: guess)
     }
     
     /*returns TRUE if GUESS is in PHRASE else FALSE */
@@ -41,8 +67,17 @@ class HangmanModel: UIViewController {
         return phrase.contains(guess)
     }
     
+    func decreaseLives() {
+        lives -= 1
+    }
+    
+    func incrCorrectLetters(indicesArr: [Int]) {
+        correctLetters += indicesArr.count
+    }
+    
     /* returns indices of GUESS in PHRASE in a list.  Also
      increments correctLETTERS */
+    
     
     
     func indicesOfLetter(letter: String) -> [Int] {
@@ -50,7 +85,6 @@ class HangmanModel: UIViewController {
         for i in 0...phrase.characters.count {
             if phrase.substring(atIndex: i) == letter {
                 indices.append(i)
-                correctLetters += 1
             }
         }
         return indices
